6152f183fee8631c81f5eaa4760adea5004e5da1
Raghu Betina <raghu@firstdraft.com>
2018-01-07 18:56:51 +0000

    Direct association between users and favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :likes,
+             :class_name => "Favorite",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
